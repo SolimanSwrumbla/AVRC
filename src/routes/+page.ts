c273c2ccdda812aaceffce050a3303767c830f13
Michi Palazzo <palazzomichi@gmail.com>
2025-07-13 16:12:16 +0200

    Adapter static

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,7 @@
 import { loadBordGamesFromCsv, topN } from "$lib";
 
+export const prerender = true;
+
 export async function load({ fetch }) {
     const games = fromAsync(loadBordGamesFromCsv(fetch, '/games.csv', '/mechanics.csv')).then((games) => topN(games, 2500, (g) => g.userRatings));
     return { games };
